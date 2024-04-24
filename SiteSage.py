79ec41b0e7bb18d14e6448b24eb7df61b0ccf316
Yash Wafekar <82889782+Yash-200@users.noreply.github.com>
2024-04-24 12:38:07 +0530

    SiteSage.py

--- SiteSage.py
+++ SiteSage.py
@@ -27,7 +27,7 @@ if "vector" not in st.session_state:
 )
     st.session_state.loader=WebBaseLoader("https://www.basicphone.in/")
     st.session_state.docs=st.session_state.loader.load()
-    st.session_state.docs.append(process_pdf_files("/fepw1dd"))
+    st.session_state.docs.append(process_pdf_files("/docs_dir"))
 
     st.session_state.text_splitter=RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=200)    
     st.session_state.final_documents=st.session_state.text_splitter.split_documents(st.session_state.docs[:])
